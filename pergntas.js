76399be8bdecd4d0753a3990381b66c16ee2d18c
thaiane <161048256+thaiane54321@users.noreply.github.com>
2024-10-04 09:57:43 -0300

    comit

--- pergntas.js
+++ pergntas.js
@@ -45,3 +45,15 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
